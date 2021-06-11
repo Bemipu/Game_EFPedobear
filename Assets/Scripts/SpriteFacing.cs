ca785a20a3287a0568376205eefc86ce814ebb55
bemipu <bemipu@outlook.com>
2021-06-11 19:20:31 +0800

    testing AI and MAP

---
+++ Assets/Scripts/SpriteFacing.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteFacing : MonoBehaviour
+{
+
+    public Camera cameraToLookAt;
+    private Transform cam_pos;
+
+    void Start(){
+        cam_pos = new GameObject().transform;
+    }
+
+    void Update()
+    {
+        //cam_pos = cameraToLookAt.transform;
+        cam_pos.position = new Vector3(cameraToLookAt.transform.position.x,transform.position.y,cameraToLookAt.transform.position.z);
+        transform.LookAt(cam_pos);
+    }
+    
+}
